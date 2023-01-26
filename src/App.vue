03383d81197d9b728973f77bb28c4a213ec46ea1
Gabi <gabyszabo5@gmail.com>
2023-01-26 14:38:16 +0100

    adding html and css

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,118 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <!-- <div class="overlay">
+      <div class="modal">
+        <textarea name="note" id="note" cols="30" rows="10"></textarea>
+        <button>Add Note</button>
+        <button class="close">Close</button>
+      </div>
+    </div> -->
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button>+</button>
+      </header>
+  <div class="cards-container">
+    <div class="card">
+      <p class="main-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos amet magni nostrum. Dicta enim doloremque hic.</p>
+      <p class="date">19/07/2021</p>
+    </div>
+    <div class="card">
+      <p class="main-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos amet magni nostrum. Dicta enim doloremque hic.</p>
+      <p class="date">19/07/2021</p>
+    </div>
+  </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+main {
+  height: 100vh;
+  width: 100vw;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+.container {
+  max-width: 1000px;
+  padding: 10px;
+  margin: 0 auto;
+  }
 
-@media (min-width: 1024px) {
   header {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    justify-content: space-between;
+    align-items: center;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  h1 {
+    font-weight: bold;
+    margin-bottom: 25px;
+    font-size: 75px;
   }
 
-  header .wrapper {
+  header-button {
+    border: none;
+    padding: 10px;
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+    background-color: rgb(21,20,20);
+    border-radius: 100;
+    color: white;
+    font-size: 20px;
+  }
+
+  .card {
+    width: 225px;
+    height:225px;
+    background-color: rgb(237, 182, 44);
+    padding: 10px;
+    border-radius: 15px;
     display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-right: 20px;
+    margin-bottom: 20px;
+
   }
-}
+  .date {
+    font-size: 12px;
+  }
+  .cards-container {
+display: flex;
+flex-wrap: wrap;
+  }
+  .overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,0.77);
+    z-index: 10;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .modal {
+    width: 750px;
+    background-color: white;
+    border-radius: 10px;
+    padding: 30px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+  }
+  .modal button {
+    padding: 10px 20px;
+    font-size: 20px;
+    width: 100%;
+    background-color: skyblue;
+    border: none;
+    color: white;
+    cursor: pointer;
+    margin-top: 10px;
+  }
+
+  .modal .close {
+    background-color: rebeccapurple;
+  }
 </style>
