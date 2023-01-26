6946c90d158a7d018735b13f52b5ccde1532c296
Gabi <gabyszabo5@gmail.com>
2023-01-26 16:04:18 +0100

    added events

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,61 @@
+<script setup>
+import {ref} from "vue"
+
+const showModal = ref(false)
+const newNote = ref("")
+const errorMessage = ref("")
+const notes = ref([])
+
+function getRandomColor() {
+  const color = "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+  return color;
+}
+
+const addNote = () => {
+  if(newNote.value.length <10) {
+    return errorMessage.value = "Note needs to be 10 characters or more"
+  }
+  notes.value.push({
+    id: Math.floor(Math.random() * 100000),
+    text: newNote.value,
+    color: getRandomColor(),
+    date: new Date()
+  })
+  showModal.value = false;
+  newNote.value = ""
+}
+</script>
+
 <template>
   <main>
-    <!-- <div class="overlay">
+    <div v-if="showModal" class="overlay">
       <div class="modal">
-        <textarea name="note" id="note" cols="30" rows="10"></textarea>
-        <button>Add Note</button>
-        <button class="close">Close</button>
+        <p @click="showModal = false">x</p>
+        <textarea v-model="newNote"/>
+        <p class="errormsg" v-if="errorMessage">{{ errorMessage }}</p>
+        <button @click="addNote">Add Note</button>
       </div>
-    </div> -->
+    </div>
     <div class="container">
       <header>
         <h1>Notes</h1>
-        <button>+</button>
+        <button @click="showModal = true">+</button>
       </header>
-  <div class="cards-container">
-    <div class="card">
-      <p class="main-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos amet magni nostrum. Dicta enim doloremque hic.</p>
-      <p class="date">19/07/2021</p>
-    </div>
-    <div class="card">
-      <p class="main-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos amet magni nostrum. Dicta enim doloremque hic.</p>
-      <p class="date">19/07/2021</p>
-    </div>
-  </div>
+      <div class="cards-container">
+        <div v-for="note in notes" class="card" :style="{backgroundColor: note.color}">
+          <p class="main-text">{{ note.text }}</p>
+          <p class="date">{{ note.date.toLocaleDateString("en-US") }}</p>
+        </div>
+      </div>
     </div>
   </main>
 </template>
 
 <style scoped>
-main {
-  height: 100vh;
-  width: 100vw;
-}
-
-.container {
-  max-width: 1000px;
-  padding: 10px;
-  margin: 0 auto;
-  }
-
-  header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  .container {
+    max-width: 1000px;
+    padding: 10px;
+    margin: 0 auto
   }
 
   h1 {
@@ -50,21 +64,9 @@ main {
     font-size: 75px;
   }
 
-  header-button {
-    border: none;
-    padding: 10px;
-    width: 50px;
-    height: 50px;
-    cursor: pointer;
-    background-color: rgb(21,20,20);
-    border-radius: 100;
-    color: white;
-    font-size: 20px;
-  }
-
   .card {
     width: 225px;
-    height:225px;
+    height: 225px;
     background-color: rgb(237, 182, 44);
     padding: 10px;
     border-radius: 15px;
@@ -73,25 +75,57 @@ main {
     justify-content: space-between;
     margin-right: 20px;
     margin-bottom: 20px;
-
   }
+
+  .main-text {
+    line-height: 1.25;
+    font-size: 17.5px;
+    font-weight: bold;
+  }
+
   .date {
-    font-size: 12px;
+    font-size: 12.5px;
+    margin-top: auto;
+  }
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  header button {
+    border: none;
+    padding: 10px;
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+    background-color: rgb(21, 20, 20);
+    border-radius: 1000px;
+    color: white;
+    font-size: 20px;
   }
   .cards-container {
-display: flex;
-flex-wrap: wrap;
+    display: flex;
+    flex-wrap: wrap;
   }
+
   .overlay {
     position: absolute;
     width: 100%;
     height: 100%;
-    background-color: rgba(0,0,0,0.77);
-    z-index: 10;
+    background-color: rgba(0, 0, 0, 0.77);
     display: flex;
     align-items: center;
     justify-content: center;
+    z-index: 10;
   }
+
+  main {
+    height: 100vh;
+    width: 100vw;
+  }
+
   .modal {
     width: 750px;
     background-color: white;
@@ -101,18 +135,33 @@ flex-wrap: wrap;
     display: flex;
     flex-direction: column;
   }
+
   .modal button {
     padding: 10px 20px;
     font-size: 20px;
     width: 100%;
-    background-color: skyblue;
+    background-color: blueviolet;
     border: none;
     color: white;
     cursor: pointer;
-    margin-top: 10px;
+    margin-top: 15px;
+
   }
 
-  .modal .close {
-    background-color: rebeccapurple;
+  .modal p {
+    margin-left: auto;
+    font-size: 20px;
+    z-index: 100000;
+    cursor: pointer;
+  }
+
+  textarea {
+    width: 100%;
+    height: 200px;
+    padding: 5px;
+    font-size: 20px;
+  }
+  .errormsg {
+    color: red;
   }
 </style>
